fb7c6292c3f3972ad87a41cdac471e5591ea6cca
MecGlandorff <mpjglandorff@gmail.com>
2024-11-29 22:11:13 +0200

    Random forest classifier + added new feature engineering func

--- preprocessing.py
+++ preprocessing.py
@@ -42,3 +42,15 @@ def scale_features(data, features):
     scaler = MinMaxScaler()
     data[features] = scaler.fit_transform(data[features])
     return data
+
+def remove_outliers(data, column):
+
+    """ Remove the outliers from the columns """
+    Q1 = data[column].quantile(0.25)
+    Q3 = data[column].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    return data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
